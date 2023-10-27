13727ef1f99f7f43c20f7e7b01831b3c9c6e5867
rakeshchowdary2304 <149085689+rakeshchowdary2304@users.noreply.github.com>
2023-10-27 22:28:04 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,58 @@
+//creating an array and passing the numbers, questions, options and answers
+let questions = [
+    {
+        numb: 1,
+        question: "who is the father of our nation?",
+        answer: "Mahatma Gandhi",
+        options: [
+            "Abdul kalam",
+            "Mahatma Gandhi",
+            "Nehru",
+            "Narendra modi"
+        ]
+    },
+    {
+        numb: 2,
+        question: "who was the first president of india?",
+        answer: "Dr.Rajendra prasad",
+        options: [
+            "Dr.Rajendra prasad",
+            "Ramnath kovindh",
+            "Gandhi",
+            "Ambedhkar"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is longest river on the earth?",
+        answer: "Nile",
+        options: [
+            "Krishna",
+            "Godavari",
+            "Kaveri",
+            "Nile"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Shape of egg is?",
+        answer: "oval",
+        options: [
+            "sphere",
+            "circle",
+            "oval",
+            "semi circle"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which organ purify our blood?",
+        answer: "kidney",
+        options: [
+            "lungs",
+            "kidney",
+            "heart",
+            "stomach"
+        ]
+    },
+];
